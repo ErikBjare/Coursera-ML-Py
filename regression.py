ecc6b24fcb9c5f325cbd925d0ce4f1c2b2c9c82b
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-04-19 17:11:09 +0200

    Cleaned up some, still got a few nasty bugs...

--- regression.py
+++ regression.py
@@ -3,22 +3,25 @@ import unittest
 
 def linear_hypothesis(x, theta):
     """The linear hypothesis"""
-    return dot(x, theta)
+    return dot(theta.T, x)
 
 def computeCost(x, y, theta, reg=None):
     """Does the same thing as computeCost.m and computeCostMulti.m"""
     m = len(x)
-    J = multiply(1/(2*m), power((linear_hypothesis(x, theta)-y), 2).sum())
+    diff_squared = square(linear_hypothesis(x, theta)-y)
+    print("diff_squared = ", diff_squared)
+    J = 1/(2*m) * diff_squared.sum()
+    print("J = ", J)
     if reg:
         J += regularize(reg, m, theta)
     return J
 
 def normalEqn(x, y):
     """The normal equation"""
-    return dot(
-               linalg.pinv(dot(x.T, x)),
-               dot(x.T,
-                   y))
+    s1 = linalg.pinv(dot(x.T, x))
+    s2 = dot(x, y)
+    s3 = s1 * s2
+    return s3
 
 def regularize(reg, m, theta):
     """Regularization"""
@@ -35,33 +38,44 @@ class RegressionTest(unittest.TestCase):
             self.xs.append(x)
             self.ys.append(y)
             self.thetas.append(theta)
-            #print("X:\n", x)
-            #print("Y:\n", y)
-            #print("Theta:\n", theta)
+            print("Case", len(self.xs)-1)
+            print("X:\n", x)
+            print("Y:\n", y)
+            print("Theta:\n", theta)
 
-        x = matrix([[1, 2]]).T
-        y = matrix([[1, 2]]).T
-        theta = matrix([1])
+        x = array([[1], [2]])
+        y = array([[5]])
+        theta = array([[1], [2]])
         add_set(x, y, theta)
 
-
-        x = matrix("[[1, 1, 1, 1]; [1, 2, 2, 2]; [1, 2, 3, 3]; [1, 2, 3, 4]]")
-        y = matrix("[4; 7; 9; 10]")
-        theta = matrix([[1, 1, 1, 1]]).T
+        x = array([[1, 1, 1, 1], [1, 2, 2, 2], [1, 2, 3, 3], [1, 2, 3, 4]])
+        y = array([[4], [7], [9], [10]])
+        theta = array([[1], [1], [1], [1]])
         add_set(x, y, theta)
+
+    def test_g(self):
+        for i in range(len(self.xs)):
+            ylh = linear_hypothesis(self.xs[i], self.thetas[i]).T
+            thetane = normalEqn(self.xs[i], self.ys[i])
+            self.assertTrue(allclose(ylh, self.ys[i]),
+                    msg="ERROR on case {}\nY: {}\nY_LH: {}".format(i, self.ys[i], ylh))
+            self.assertTrue(allclose(thetane, self.thetas[i]),
+                    msg="ERROR on case {}\nTheta: {}\nTheta_NE: {}".format(i, self.thetas[i], thetane))
 
     def test_linear_cost(self):
         for i in range(len(self.xs)):
-            #print("Trying case", i)
             j = computeCost(self.xs[i], self.ys[i], self.thetas[i])
-            self.assertEqual(j, 0)
+            self.assertEqual(j, 0, msg="ERROR on case {}".format(i))
 
     def test_normal_equation(self):
         for i in range(len(self.xs)):
-            #print("Trying case", i)
             nTheta = normalEqn(self.xs[i], self.ys[i])
-            self.assertTrue(allclose(self.thetas[i], nTheta))
+            print(self.thetas[i], nTheta)
+            self.assertTrue(allclose(self.thetas[i], nTheta),
+                    msg="ERROR on case {}:\nTheta: {}\nTheta_NE: {}".format(i, self.thetas[i], nTheta))
 
 
 if __name__ == "__main__":
+    #print(x, theta)
+
     unittest.main()
